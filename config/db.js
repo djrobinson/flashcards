4a496ecba5d97a799c045d66a61e35209db567e7
Danny Robinson <daniel@djrobinson.me>
2016-01-24 21:57:09 -0700

    DB configs

--- config/db.js
+++ config/db.js
@@ -1,4 +1,4 @@
 
 module.exports = {
-	url : 'mongodb://drob01:Secret1!@apollo.modulusmongo.net:27017/yBo8nery'
+	url : 'mongodb://heroku_f260h22k:jku2imq6be4oc7fiihh9lnivst@ds061721.mongolab.com:61721/heroku_f260h22k'
 }
